614bce78b05af50a0c2633f1aae169bde84d9ae0
Gökhan Durmaz <>
2020-12-18 13:56:44 +0300

    #1003 Fixing bug issue  Fix field errors, missing annotations, missing impl. of abstracts ,and wrong func. parameters

--- app/src/main/java/com/qtmwex/demo/data/artist/ArtistData.kt
+++ app/src/main/java/com/qtmwex/demo/data/artist/ArtistData.kt
@@ -1,13 +1,14 @@
 package com.qtmwex.demo.data.artist
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 /**
  * Created by gokhan on 12/15/20.
  */
 @Entity(tableName = "Artists")
 data class ArtistData(
-    val id: Int,
+    @PrimaryKey(autoGenerate = true) val Id: Int,
     val name: String,
     val albumCount: Int,
     val songCount: Int
